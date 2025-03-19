ac78e8bf49696813bf8a27cbd5b1ec1ba0e8c7be
Styles <stylesk11@gmail.com>
2025-03-19 16:32:44 -0700

    set up config file w/ env

---
+++ app.config.js
@@ -0,0 +1,32 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "poplr",
+    slug: "poplr",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      TMDB_API_KEY: process.env.TMDB_API_KEY
+    }
+  }
+};
